281faa3be254abab9391272c585d7f9ce84c1385
Alice Mao <amao@wustl.edu>
2023-02-13 14:43:52 -0600

    baseline

---
+++ scripts/ts/dynamic-dom/schedule.ts
@@ -0,0 +1,65 @@
+export function showInfoTeddy() {
+    var x = document.getElementById("teddy_bears");
+    if (x !== null) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
+
+export function showInfoCubs() {
+    var x = document.getElementById("bear_cubs");
+    if (x !== null) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
+
+export function showInfoBears() {
+    var x = document.getElementById("big_bears");
+    if (x !== null) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
+
+export function showInfoPanda() {
+    var x = document.getElementById("panda_bear");
+    if (x !== null) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
+
+export function showInfoSun() {
+    var x = document.getElementById("sun_bear");
+    if (x !== null) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
+
+export function showInfoTracks() {
+    var x = document.getElementById("bear_tracks");
+    if (x !== null) {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+}
